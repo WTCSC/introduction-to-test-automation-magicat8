d2f828def2ea72682e4c48f55fe3db301090a200
Cameron <magicat8bill@gmail.com>
2024-09-09 14:12:24 -0600

    added tests

--- test_math_it_up.py
+++ test_math_it_up.py
@@ -1,5 +1,5 @@
 import pytest
-import math_it_up
+from math_it_up import is_even, is_odd, mean, median, mode
 
 """
 This file contains the tests for the math_it_up module, which contains the
@@ -26,28 +26,37 @@ the mode of the numbers.
 To run the tests, run `pytest` from the command line in the same directory as
 this file.
 """
-
-def test_is_even():
+@pytest.mark.parametrize("number1, expected", [(4, True),(3, False),(6, True)])
+def test_is_even(number1, expected):
   """
   Tests for the `is_even` function
   """
+  assert is_even(number1) == expected
 
-def test_is_odd():
+@pytest.mark.parametrize("number2, expected", [(3, True),(2, False),(7, True)])
+def test_is_odd(number2, expected):
   """
   Tests for the `is_odd` function
   """
+  assert is_odd(number2) == expected
 
-def test_mean():
+@pytest.mark.parametrize("number_list, expected", [([2,3,5,7,9,8,3], 5.285714285714286)])
+def test_mean(number_list, expected):
   """
   Tests for the `mean` function
   """
+  assert mean(number_list) == expected
 
-def test_median():
+@pytest.mark.parametrize("number_list1, expected", [([2,3,5,7,9,8,3], 5)])
+def test_median(number_list1, expected):
   """
   Tests for the `median` function
   """
+  assert median(number_list1) == expected
 
-def test_mode():
+@pytest.mark.parametrize("number_list2, expected", [([2,3,5,7,9,8,3], [3])])
+def test_mode(number_list2, expected):
   """
   Tests for the `mode` function
   """
+  assert mode(number_list2) == expected
