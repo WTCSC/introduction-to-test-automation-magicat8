582f41a8a4c04440e2e91db30873c14c5a79b783
Cameron <magicat8bill@gmail.com>
2024-09-09 14:29:23 -0600

    Added tests

--- test_math_it_up.py
+++ test_math_it_up.py
@@ -26,37 +26,33 @@ the mode of the numbers.
 To run the tests, run `pytest` from the command line in the same directory as
 this file.
 """
-@pytest.mark.parametrize("number1, expected", [(4, True),(3, False),(6, True)])
-def test_is_even(number1, expected):
+def test_is_even():
   """
   Tests for the `is_even` function
   """
-  assert is_even(number1) == expected
+  assert is_even(4) == True
 
-@pytest.mark.parametrize("number2, expected", [(3, True),(2, False),(7, True)])
-def test_is_odd(number2, expected):
+def test_is_odd():
   """
   Tests for the `is_odd` function
   """
-  assert is_odd(number2) == expected
+  assert is_odd(3) == True 
 
-@pytest.mark.parametrize("number_list, expected", [([2,3,5,7,9,8,3], 5.285714285714286)])
-def test_mean(number_list, expected):
+
+def test_mean():
   """
   Tests for the `mean` function
   """
-  assert mean(number_list) == expected
+  assert mean([2,3,5,7,9,8,3]) == 5.285714285714286
 
-@pytest.mark.parametrize("number_list1, expected", [([2,3,5,7,9,8,3], 5)])
-def test_median(number_list1, expected):
+def test_median():
   """
   Tests for the `median` function
   """
-  assert median(number_list1) == expected
+  assert median([2,3,5,7,9,8,3]) == 5
 
-@pytest.mark.parametrize("number_list2, expected", [([2,3,5,7,9,8,3], [3])])
-def test_mode(number_list2, expected):
+def test_mode():
   """
   Tests for the `mode` function
   """
-  assert mode(number_list2) == expected
+  assert mode([2,3,5,7,9,8,3]) == [3]
